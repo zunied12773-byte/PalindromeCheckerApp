5833164adf5582b114bcebab63a315bc45c5ddd9
zunied12773-byte <zunied12773@gmail.com>
2026-03-10 15:43:49 +0530

    UC13: Performance Comparison

--- PalindromeCheckerApp2/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp2/src/PalindromeCheckerApp.java
@@ -12,36 +12,27 @@ public class PalindromeCheckerApp {
 
         String input = "level";
 
-        PalindromeStrategy strategy = new StackStrategy();
+        long start = System.nanoTime();
 
-        boolean result = strategy.check(input);
+        boolean isPalindrome = true;
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        long end = System.nanoTime();
+        long time = end - start;
 
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
-}
-
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + time + " ns");
     }
 }
 
